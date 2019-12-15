7407469809134d9a278d80bd695647919847c448
Paweł Pyc <pawelpyc154@gmail.com>
2019-12-15 23:35:04 +0100

    Updates

---
+++ precache-manifest.fd376821732ec559176f216656cdf36e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827d1de85194a25ae60ea29707b92d64",
+    "url": "/Project-7/index.html"
+  },
+  {
+    "revision": "c0d293c427d46da72c4d",
+    "url": "/Project-7/static/js/2.6c710b1a.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/Project-7/static/js/2.6c710b1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2057cf54172c26d802f8",
+    "url": "/Project-7/static/js/main.662945f4.chunk.js"
+  },
+  {
+    "revision": "4a312fdfebb4ee3d0d97",
+    "url": "/Project-7/static/js/runtime-main.557bb788.js"
+  },
+  {
+    "revision": "acf566a7dbada9883de71208f9ce72bd",
+    "url": "/Project-7/static/media/Drone@2x.acf566a7.png"
+  },
+  {
+    "revision": "b1676a1510cef3d5e68186adbe5df795",
+    "url": "/Project-7/static/media/Layer_2.b1676a15.png"
+  },
+  {
+    "revision": "01faf6e5cace783d5c4aec0ff47940a2",
+    "url": "/Project-7/static/media/Layer_3.01faf6e5.png"
+  },
+  {
+    "revision": "50d29ca684471431184dec6644a736a5",
+    "url": "/Project-7/static/media/Layer_4.50d29ca6.png"
+  },
+  {
+    "revision": "757b30417ef37ef184c810c6f5dc4f1b",
+    "url": "/Project-7/static/media/Layer_5_eb_pattern.757b3041.png"
+  },
+  {
+    "revision": "4007bbec58bf40998244df0ef7721784",
+    "url": "/Project-7/static/media/drone_1.4007bbec.png"
+  }
+]);
